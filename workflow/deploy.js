50fa1179f5200c86261d993dbadaa5302e05a34e
Chris Roberson <chrisronline@gmail.com>
2015-02-24 14:50:17 -0500

    Adding deploy-stage task

--- workflow/deploy.js
+++ workflow/deploy.js
@@ -14,5 +14,20 @@ plugin.attach = function(gulp, config) {
       .pipe(publisher.cache())
       .pipe(awspublish.reporter());
   });
+  gulp.task('deploy-stage', function() {
+    // https://github.com/pgherveou/gulp-awspublish
+    var awspublish = require('gulp-awspublish');
+    var _ = require('lodash');
+    var credentials = _.extend({}, config.credentials);
+    credentials.bucket = credentials.stage_bucket;
+    var publisher = awspublish.create(credentials);
+    var headers = {};
+    return gulp.src('dist/**/*')
+      .pipe(awspublish.gzip())
+      .pipe(publisher.publish(headers))
+      .pipe(publisher.sync()) // delete other files?
+      .pipe(publisher.cache())
+      .pipe(awspublish.reporter());
+  });
 };
 module.exports = plugin;
